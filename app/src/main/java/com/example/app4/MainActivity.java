fc6a95c9eb9870d8eda3153fcb26246b37f7fed5
Alumno <josewilemerhamachi@gmail.com>
2022-04-25 12:13:36 -0400

    Second Commit App4 Verify Vocals using ArrayList and characters with verify

--- app/src/main/java/com/example/app4/MainActivity.java
+++ app/src/main/java/com/example/app4/MainActivity.java
@@ -8,15 +8,16 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.BooleanSupplier;
 
 public class MainActivity extends AppCompatActivity {
     private EditText ETword;
     private Button BTNprocess, BTNcount;
     private CheckBox CBlowerCase;
-    private String word, message;
+    private String word;
     private Boolean isLowerCase;
-    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,36 +29,43 @@ public class MainActivity extends AppCompatActivity {
 
     private void countVocals() {
         catchInformation();
-        if (word.contains("a") || word.contains("A")){
-            counter += 1;
+        int counter = 0;
+        if ( word.length() > 0) {
+            ArrayList<Character> vocals =  new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+            for (int i = 0; i < word.length(); i++) {
+                // if (vocals.contains(word.toLowerCase().charAt(i))) counter++; // Sin usar el metodo de convertir a minusculas
+                if (vocals.contains(word.charAt(i))) counter++;
+            }
+            Toast.makeText(this,"La palabra tiene " + counter + " vocales",Toast.LENGTH_LONG).show();
         }
-        if (word.contains("e") || word.contains("E")){
-            counter += 1;
+        else Toast.makeText(this,"El espacio para verificar vocales esta vacio",Toast.LENGTH_LONG).show();
+        /* if ( word.length() > 0) {
+            for (int i = 0; i < word.length(); i++) {
+                char letra = word.charAt(i);
+                if (letra == 'a' || letra == 'A') counter += 1;
+                if (letra == 'e' || letra == 'E') counter += 1;
+                if (letra == 'i' || letra == 'I') counter += 1;
+                if (letra == 'o' || letra == 'O') counter += 1;
+                if (letra == 'u' || letra == 'U') counter += 1;
+            }
+            Toast.makeText(this,"La palabra tiene " + counter + " vocales",Toast.LENGTH_LONG).show();
         }
-        if (word.contains("i") || word.contains("I")){
-            counter += 1;
-        }
-        if (word.contains("o") || word.contains("O")){
-            counter += 1;
-        }
-        if (word.contains("u") || word.contains("U")){
-            counter += 1;
-        }
-        message = "La palabra tiene " + this.counter + " vocales";
-        counter = 0;
-        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+        else Toast.makeText(this,"El espacio para verificar vocales esta vacio",Toast.LENGTH_LONG).show();*/
     }
 
     private void catchInformation() {
         word = ETword.getText().toString();
         isLowerCase = CBlowerCase.isChecked();
+        changeWordLowerCase();
+    }
+
+    private void changeWordLowerCase() {
+        if (isLowerCase) word = word.toLowerCase();
     }
 
     private void initViews() {
         ETword = findViewById(R.id.ETword);
-
         CBlowerCase = findViewById(R.id.CBlowercase);
-
         BTNprocess = findViewById(R.id.BTNprocess);
         BTNcount = findViewById(R.id.BTNcount);
     }
